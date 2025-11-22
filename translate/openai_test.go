95336893f01f28a464f25b3f1d395ad53782311b
GouJie <507762441@qq.com>
2025-11-23 04:22:41 +0800

    up

--- translate/openai_test.go
+++ translate/openai_test.go
@@ -51,40 +51,4 @@ func TestNewAiTran(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 	t.Log(s)
-	s, err = o.T(&TranReq{
-		From: "英语",
-		To:   "法语",
-		Paras: []string{
-			"Open an existing docx/pptx/xlsx document	11",
-			"OpenXML concepts	12",
-			"Specification versions	13",
-			"Architecture	13",
-			"Jaxb: marshalling and unmarshalling	15",
-			"Parts List	16",
-			"MainDocumentPart	18",
-			"Samples	19",
-		},
-	})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	t.Log(s)
-	s, err = o.T(&TranReq{
-		From: "英语",
-		To:   "日语",
-		Paras: []string{
-			"Open an existing docx/pptx/xlsx document	11",
-			"OpenXML concepts	12",
-			"Specification versions	13",
-			"Architecture	13",
-			"Jaxb: marshalling and unmarshalling	15",
-			"Parts List	16",
-			"MainDocumentPart	18",
-			"Samples	19",
-		},
-	})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	t.Log(s)
 }
