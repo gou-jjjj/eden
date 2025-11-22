04763a037765ec55870ba57847bbbb8d7d3df2f9
GouJie <507762441@qq.com>
2025-11-22 23:45:43 +0800

    up

--- translate/openai_test.go
+++ translate/openai_test.go
@@ -5,52 +5,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/gou-jjjj/eden/lang"
 	"github.com/tmc/langchaingo/llms/ollama"
 )
-
-func TestTranOpenai_T(t1 *testing.T) {
-	type args struct {
-		req *TranReq
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "test1",
-			args: args{
-				req: &TranReq{
-					From: lang.EN,
-					To:   lang.ZH,
-					Paras: Paragraph{
-						"Hello, how are you?",
-						"Today is a sunny day.",
-						"OpenAI provides powerful AI models.",
-						"Let's translate these sentences.",
-						"Testing the translation functionality.",
-					},
-				},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t1.Run(tt.name, func(t1 *testing.T) {
-			s := OpenaiModelList[AliBaBa]
-			t := &TranOpenai{
-				url:   s.Url,
-				key:   s.Key,
-				model: s.Model,
-			}
-			got, err := t.T(tt.args.req)
-			if err != nil {
-				t1.Errorf("TranOpenai.T() error = %v", err)
-				return
-			}
-			t1.Log(got)
-		})
-	}
-}
 
 func TestName(t *testing.T) {
 	llm, err := ollama.New(
