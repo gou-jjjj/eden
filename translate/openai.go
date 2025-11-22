aff5ea030b97013dde92e37550fea933e4baeac5
GouJie <507762441@qq.com>
2025-11-23 03:58:05 +0800

    up

--- translate/openai.go
+++ translate/openai.go
@@ -9,29 +9,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gou-jjjj/eden/lang"
 	"github.com/tmc/langchaingo/llms"
 )
-
-const (
-	ZhiPu      = "zhipu"
-	GithubFree = "githubfree"
-	OpenRouter = "openrouter"
-	AliBaBa    = "alibaba"
-
-	Seq = "\n------------\n"
-)
-
-var OpenaiModelList = map[string]struct {
-	Url   string
-	Key   string
-	Model string
-}{
-	ZhiPu:      {"https://open.bigmodel.cn/api/paas/v4", "23c650b6b73d4b1b80500699edcbf87c.qU9lcBTeGfku2gKz", "glm-4-plus"},
-	GithubFree: {"https://api.chatanywhere.tech", "sk-vINYqBzbzrhdsFxZCO7MSSEvHL8tPradBhl77tLmWmEoTXs5", "deepseek-v3"},
-	OpenRouter: {"https://openrouter.ai/api/v1", "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", "x-ai/grok-4-fast:free"},
-	AliBaBa:    {"https://dashscope.aliyuncs.com/compatible-mode/v1", "sk-227cf58d893d4a689e82d2b8eb8f3564", "qwen-plus"},
-}
 
 type AiTran struct {
 	retry int
@@ -156,7 +135,7 @@ func (t *AiTran) splitFlow(req *TranReq) (string, error) {
 	})
 
 	content := fmt.Sprintf(prompt, data)
-	call, err := t.call(content, llms.WithJSONMode())
+	call, err := t.call(content, llms.WithJSONMode(), llms.WithTemperature(0.5))
 	if err != nil {
 		return "", err
 	}
@@ -177,7 +156,7 @@ func (t *AiTran) addLog(req *TranReq, res []string) {
 		} else {
 			s.WriteString("[] ")
 		}
-		s.WriteString(Seq)
+		s.WriteString("\n-----------------------------\n")
 	}
 
 	_ = os.WriteFile(
@@ -221,24 +200,3 @@ func (t *AiTran) nextModel() llms.Model {
 	t.mIdx++
 	return t.model()
 }
-
-func getLangKey(from, to string) string {
-	return fmt.Sprintf("%s_%s", from, to)
-}
-
-var samplePrompt = map[string][]llms.MessageContent{
-	getLangKey(lang.ZH, lang.EN): {
-		llms.TextParts(llms.ChatMessageTypeHuman, "要运行程序，请使用：`python main.py --input data.json`\n---\n这将处理数据集并生成\n---\n输出文件到`/results/`目录，截止东部时间下午5点。"),
-		llms.TextParts(llms.ChatMessageTypeAI, "To run the program, use: `python main.py --input data.json`\n---\nThis will process the dataset and generate\n---\noutput files in `/results/` directory by 5PM EST."),
-	},
-	getLangKey(lang.EN, lang.ZH): {
-		llms.TextParts(llms.ChatMessageTypeHuman, "To run the program, use: `python main.py --input data.json`\n---\nThis will process the dataset and generate\n---\noutput files in `/results/` directory by 5PM EST."),
-		llms.TextParts(llms.ChatMessageTypeAI, "要运行程序，请使用：`python main.py --input data.json`\n---\n这将处理数据集并生成\n---\n输出文件到`/results/`目录，截止东部时间下午5点。"),
-	},
-}
-
-const (
-	translatePrompt = `
-你是一个机器翻译专家，精通多种语言之间的翻译。请将以下内容从 %s 翻译成 %s 。请确保翻译准确且符合目标语言的语法和文化习惯。
-`
-)
